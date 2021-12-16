e6440e1c049ea9c8752fffc69335152151155d9b
Achim <a.grether@hotmail.com>
2021-12-16 18:56:19 +0100

    added functions for ToDo-Component -> remove Todo -> toggleTodo (status) -> changeTodo (status) -> changeText from Todo

--- src/app/todos/components/todo/todo.component.ts
+++ src/app/todos/components/todo/todo.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter, OnInit, ViewChild, ElementRef, OnChanges, SimpleChanges } from "@angular/core";
+import { TodosService } from "../../services/todos.service";
 import { TodoInterface } from "../../types/todo.interface";
 
 @Component({
@@ -6,18 +7,47 @@ import { TodoInterface } from "../../types/todo.interface";
         templateUrl: './todo.component.html',
 })
 
-export class TodoComponent {
+export class TodoComponent implements OnInit, OnChanges {
         @Input('todo') todoProps: TodoInterface | undefined
         @Input('isEditing') isEditingProps: boolean;
         @Output('setEditingId') setEditingIdEvent: EventEmitter<
                 string | null> = new EventEmitter();
+        editingText: string = '';
+        @ViewChild('textInput') textInput: ElementRef;
+
+        constructor(private todosService: TodosService) {}
+
+        ngOnInit(): void {
+            this.editingText = this.todoProps.text;
+        }
+
+        ngOnChanges(changes: SimpleChanges): void {
+            if (changes["isEditingProps"].currentValue) {
+                    setTimeout(() => {
+                        this.textInput.nativeElement.focus();
+                    }, 0);
+            }
+        }
 
         setTodoInEditMode(): void {
-                console.log("Set in Edit Mode");
                 this.setEditingIdEvent.emit(this.todoProps.id);
         }
 
         removeTodo(): void {
-                console.log('removeTodo');
+                this.todosService.removeTodo(this.todoProps.id);
+        }
+
+        toggleTodo(): void {
+                this.todosService.toggleTodo(this.todoProps.id);
+        }
+
+        changeText(event: Event): void {
+                const value = (event.target as HTMLInputElement).value;
+                this.editingText = value;
+        }
+
+        changeTodo(): void {
+                this.todosService.changeTodo(this.todoProps.id, this.editingText);
+                this.setEditingIdEvent.emit(null);
         }
 }
